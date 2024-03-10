241e4fc076e0d44c4cc721167d69e25b5629d339
Diego Santana <3c.santana@gmail.com>
2024-03-10 20:25:57 -0300

    Primeira solução: Implementando a interface funcional Consumer em uma classe separada

---
+++ 79-revisao-programacao-funcional/03-consumer/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.MyConsumer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("TV", 900.0));
+		products.add(new Product("Mouse", 50.0));
+		products.add(new Product("Tablet", 350.50));
+		products.add(new Product("HD Case", 80.90));
+		
+		products.forEach(System.out::println);
+		System.out.println();
+		
+		products.forEach(new MyConsumer());
+		products.forEach(System.out::println);
+	}
+
+}
