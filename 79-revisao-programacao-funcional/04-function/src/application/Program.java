7dbe15c90304ab5a6500c98d6cde6e8e60fdcc22
Diego Santana <3c.santana@gmail.com>
2024-03-11 10:13:34 -0300

    Solução 1: Implemento a Function em uma classe separada

---
+++ 79-revisao-programacao-funcional/04-function/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCase;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		List<Product> products = new ArrayList<Product>();
+		products.add(new Product("Tv", 900.00));
+		products.add(new Product("Mouse", 50.00));
+		products.add(new Product("Tablet", 350.50));
+		products.add(new Product("HD Case", 80.90));
+		
+		products.forEach(System.out::println);
+		System.out.println();
+		
+		List<String> names = products.stream().map(new UpperCase()).collect(Collectors.toList());;
+		names.forEach(System.out::println);
+	}
+
+}
