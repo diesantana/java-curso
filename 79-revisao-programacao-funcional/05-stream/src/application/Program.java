e100e53f19121cfe000d1e404d542301348e140c
Diego Santana <3c.santana@gmail.com>
2024-03-12 17:10:18 -0300

    Aula de revisão sobre stream

---
+++ 79-revisao-programacao-funcional/05-stream/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Integer> numbers = Arrays.asList(3, 4, 5, 10, 7);
+		
+		
+	}
+}
